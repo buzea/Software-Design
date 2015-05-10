b7e440ee367e35da1a78bc7cbdfaeb634fc6d702
Vlad <buzea.vlad@gmail.com>
2015-05-10 17:47:07 +0300

    add composite

---
+++ Assignment4/src/Composite/FilledOval.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Composite;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Mihai
+ */
+public class FilledOval implements Shape{
+
+    private Graphics g;
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+    private Color color;
+
+    public FilledOval(Graphics g,int x1, int y1, int x2, int y2, Color color)
+    {
+        this.g = g;
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+        this.color = color;
+    }
+
+    public void drawShape(Graphics dragGraphics) {
+        int x, y;  // Top left corner of rectangle that contains the figure.
+         int w, h;  // Width and height of rectangle that contains the figure.
+         if (x1 >= x2) {  // x2 is left edge
+            x = x2;
+            w = x1 - x2;
+         }
+         else {          // x1 is left edge
+            x = x1;
+            w = x2 - x1;
+         }
+         if (y1 >= y2) {  // y2 is top edge
+            y = y2;
+            h = y1 - y2;
+         }
+         else {          // y1 is top edge.
+            y = y1;
+            h = y2 - y1;
+         }
+         dragGraphics.setColor(color);
+        dragGraphics.fillOval(x, y, w, h);
+    }
+
+    public Shape[] explodeShape() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ Assignment4/src/Composite/Shape.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Composite;
+
+import java.awt.Graphics;
+
+
+
+public interface Shape {
+
+	
+	public void drawShape(Graphics dragGraphics);
+
+
+}
